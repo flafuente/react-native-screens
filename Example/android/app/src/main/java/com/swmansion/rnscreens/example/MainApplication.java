e7825a0488d40496516929f7dcaf64a1b33cb797
marcgirbau <marc.girbaub@gmail.com>
2025-02-11 12:52:52 +0100

    upload library

---
+++ Example/android/app/src/main/java/com/swmansion/rnscreens/example/MainApplication.java
@@ -0,0 +1,67 @@
+package com.swmansion.rnscreens.example;
+
+import android.app.Application;
+
+import com.facebook.react.ReactApplication;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+import com.swmansion.rnscreens.RNScreensPackage;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class MainApplication extends Application implements ReactApplication {
+
+  private static class MyPackage implements ReactPackage {
+
+    @Override
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+      return Collections.emptyList();
+    }
+
+    @Override
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+      return Arrays.<ViewManager>asList(new SampleLifecycleAwareViewManager());
+    }
+  }
+
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    @Override
+    public boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+          new MainReactPackage(),
+            new RNGestureHandlerPackage(),
+          new RNScreensPackage(),
+          new MyPackage()
+      );
+    }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
+    }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+  }
+}
